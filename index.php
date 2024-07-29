dcfab239bbdd5de97abdf65d9654060038d1b681
sandeep987444 <987444@win.edu.au>
2024-07-29 16:17:05 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My page</title>
+</head>
+<body>
+<?php
+    $name ="Sandeep giri";
+    echo "My favourite lecturer is " . $name;
+?>
+</body>
+</html>
