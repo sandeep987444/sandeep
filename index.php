5517b50032a64981b05c11d0b4f73d394ec6e826
sandeep987444 <987444@win.edu.au>
2024-08-05 16:25:26 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -6,3 +6,15 @@ if (!isset($_SESSION['user']) || $_SESSION['user'] !== 'shopping') {
     exit();
 }
 ?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shopping Page</title>
+</head>
+<body>
+    <h2>Welcome to the Shopping Page</h2>
+    <p>You are logged in as a Shopper.</p>
+    <a href="../logout.php">Logout</a>
+</body>
+</html>
